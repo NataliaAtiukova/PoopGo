e0dc0994c6460eaff7280e4ce56ef28c232867b0
NatalyaAtyukova <atukowa.natalya@yandex.ru>
2025-09-06 23:00:40 +0300

    iOS: set bundle id com.app.poopgo; Info.plist display name + privacy descriptions; Podfile iOS 13; update google-services package_name; prepare iOS build

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.poopgo"
+    namespace = "com.app.poopgo"
     compileSdk = flutter.compileSdkVersion
     // Let Flutter handle NDK version automatically
 
@@ -24,7 +24,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.poopgo"
+        applicationId = "com.app.poopgo"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         // Firebase Auth 23.x requires minSdk >= 23
